c94e94933e76365360f4bb97d3e21d001826e289
Gustav Schauwecker <service@simpago.com>
2024-11-26 07:41:06 +0100

    Change layout

--- src/gui.rs
+++ src/gui.rs
@@ -39,8 +39,53 @@ impl eframe::App for AdminGui {
         ctx.request_repaint_after_secs(0.5);
         let mut logic = self.logic.lock().unwrap();
 
+        SidePanel::left("controls")
+            .exact_width(200.0)
+            .resizable(false)
+            .show(ctx, |ui| {
+                if logic.running() {
+                    if ui.button("stop").clicked() {
+                        logic.stop();
+                    }
+                } else {
+                    if ui.button("start").clicked() {
+                        logic.start();
+                    }
+                }
+                ui.label("User:");
+                ui.text_edit_singleline(&mut self.user);
+                ui.label("Message:");
+                ui.text_edit_singleline(&mut self.message);
+                if ui.input(|i| i.key_pressed(egui::Key::Enter)) {
+                    logic.handle_chat_message(ChatMessage {
+                        message: self.message.clone(),
+                        author_name: Some(self.user.clone()),
+                        author_channel_id: self.user.clone(),
+                    });
+                    self.message = String::new();
+                }
+
+                if ui.button("add test users").clicked() {
+                    for i in 1..5 {
+                        for name in ["Alice", "Bob", "John", "Jane", "Tom"] {
+                            let user = format!("{}{}", name, i);
+                            logic.handle_chat_message(ChatMessage {
+                            author_channel_id: user.clone(),
+                            author_name: Some(user.clone()),
+                            message:
+                                "nano_1iawmcfwmmdyr7xmnordt71gpnhnao8rsk4nywq5khtmedocaj6bafk4fb8h"
+                                    .to_owned(),
+                        });
+                        }
+                    }
+                }
+                if ui.button("run raffle now").clicked() {
+                    logic.run_raffle_now(self.clock.now());
+                }
+            });
+
         SidePanel::right("viewers-panel")
-            .exact_width(300.0)
+            .exact_width(200.0)
             .resizable(false)
             .show(ctx, |ui| {
                 let participants = logic.participants();
@@ -55,48 +100,6 @@ impl eframe::App for AdminGui {
                 "{}s until raffle",
                 logic.countdown(self.clock.now()).as_secs()
             ));
-        });
-
-        TopBottomPanel::bottom("chat-input").show(ctx, |ui| {
-            if logic.running() {
-                if ui.button("stop").clicked() {
-                    logic.stop();
-                }
-            } else {
-                if ui.button("start").clicked() {
-                    logic.start();
-                }
-            }
-            ui.label("User:");
-            ui.text_edit_singleline(&mut self.user);
-            ui.label("Message:");
-            ui.text_edit_singleline(&mut self.message);
-            if ui.input(|i| i.key_pressed(egui::Key::Enter)) {
-                logic.handle_chat_message(ChatMessage {
-                    message: self.message.clone(),
-                    author_name: Some(self.user.clone()),
-                    author_channel_id: self.user.clone(),
-                });
-                self.message = String::new();
-            }
-
-            if ui.button("add test users").clicked() {
-                for i in 1..5 {
-                    for name in ["Alice", "Bob", "John", "Jane", "Tom"] {
-                        let user = format!("{}{}", name, i);
-                        logic.handle_chat_message(ChatMessage {
-                            author_channel_id: user.clone(),
-                            author_name: Some(user.clone()),
-                            message:
-                                "nano_1iawmcfwmmdyr7xmnordt71gpnhnao8rsk4nywq5khtmedocaj6bafk4fb8h"
-                                    .to_owned(),
-                        });
-                    }
-                }
-            }
-            if ui.button("run raffle now").clicked() {
-                logic.run_raffle_now(self.clock.now());
-            }
         });
 
         CentralPanel::default().show(ctx, |ui| {
